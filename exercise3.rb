9592bf2609bc96641fc721cea659cf685882d940
Brant <boshartb@gmail.com>
2015-01-20 17:23:02 -0500

    Added Age calculation

--- exercise3.rb
+++ exercise3.rb
@@ -2,3 +2,9 @@ puts "What is your name?"
 
 name = gets.chomp
 puts "Hi #{name}!"
+puts "Hey, I hope this isn't awkward but how old are you turing in 2015?"
+age = gets.chomp.to_i
+
+#1
+birth_year = 2015 - age
+puts "So that means, you were born in #{birth_year} "
