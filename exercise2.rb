d9f8189486b9c3ff5c8e769a1b31fe7d01f4bcf3
Brant <boshartb@gmail.com>
2015-01-20 16:41:36 -0500

    answer 2

--- exercise2.rb
+++ exercise2.rb
@@ -2,3 +2,11 @@ puts " Answer 1: A good tip would be 20%. Do DEVS get tips?"
 
 tip = (55 * 0.20) 
 puts " Answer 2: A good tip on $55 meal is #{tip} "
+
+
+a = 45628
+b = 7839
+puts " Answer 3: The output of 45628 times 7839 is #{a+b}"
+
+
+puts " Answer 4: #{ (true && false) || (false && true) || !(false && false)}"
