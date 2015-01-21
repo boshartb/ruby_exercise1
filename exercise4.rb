eeeba51afcf42c48381e47c53674448fb83644b6
Brant <boshartb@gmail.com>
2015-01-20 19:08:37 -0500

    final touches

--- exercise4.rb
+++ exercise4.rb
@@ -1,10 +1,11 @@
 (1..100).each do |i|
 	if i % 3 == 0
-	print "bit"
+		print "bit"
 	elsif i % 5 == 0
-	print "maker"
+		print "maker"
 	elsif i % 5 and i % 3 == 0
-	print "bitmaker"
-	else print  i
+		print "bitmaker"
+	else 
+		print  i
 	end
 end
